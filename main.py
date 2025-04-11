ec40f2a872e1ead41160d260435e177c7136ed83
BruceAGR <89883121+BruceAGR@users.noreply.github.com>
2025-04-11 15:50:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import requests
 import os
+from datetime import datetime  # 👈 importa o datetime
 
-TOKEN = os.getenv('BOT_TOKEN')         # nome do secret no GitHub
-chat_id = os.getenv('CHAT_ID')         # nome do secret no GitHub
-mensagem = '🚀 Notificação enviada do Google Colab!2'
+# Pega as variáveis de ambiente (se quiser usar Secrets do GitHub futuramente)
+TOKEN = os.getenv('BOT_TOKEN')
+chat_id = os.getenv('CHAT_ID')
+
+# Substitui com valores fixos (usado agora pro teste local)
+TOKEN = '7488164113:AAEXiG5Xl-UpYChI4ASDBmMaZS3rEIwpoUc'
+chat_id = '331126754'
+
+# Pega o horário atual formatado
+agora = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+
+# Mensagem com horário
+mensagem = f'🚀 Notificação enviada do GitHub Actions às {agora}!'
 
 url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 payload = {
@@ -16,10 +27,7 @@ res = requests.post(url, data=payload)
 print(res.status_code)
 print(res.text)
 
-print(TOKEN)
-print(chat_id)
-
 if res.status_code == 200:
-    print("✅ Mensagem enviada com sucesso2!")
+    print(mensagem)
 else:
     print("❌ Erro ao enviar:", res.text)
